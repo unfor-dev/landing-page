53a20314b887315abeea6947b10e89f03d3f52d6
unfor-dev <hamzayevasadulloh2005@gmail.com>
2025-06-09 16:07:52 +0500

    Adding project starter

---
+++ src/app/layout.js
@@ -0,0 +1,14 @@
+import './globals.css'
+
+export const metadata = {
+  title: 'Landing page',
+  description: 'Landing page',
+}
+
+export default function RootLayout() {
+  return (
+    <p>
+      Hello
+    </p>
+  )
+}
